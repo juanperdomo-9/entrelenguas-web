8ea6f63e145ca02974c1d1ddcfc2917f7d79ba22
Juan Francisco  Perdomo <juanperdomo918918@gmail.com>
2026-03-04 14:52:53 -0300

    Primer version ecommerce Entre Lenguas

---
+++ tienda/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import lista_bebidas, agregar_al_carrito, ver_carrito, eliminar_del_carrito, restar_del_carrito, sumar_del_carrito
+
+urlpatterns = [
+    path('', lista_bebidas, name='lista_bebidas'),
+    path('agregar/<int:bebida_id>/', agregar_al_carrito, name='agregar_al_carrito'),
+]
+
+urlpatterns = [
+    path('', lista_bebidas, name='lista_bebidas'),
+    path('agregar/<int:bebida_id>/', agregar_al_carrito, name='agregar_al_carrito'),
+    path('carrito/', ver_carrito, name='ver_carrito'),
+    path('eliminar/<int:bebida_id>/', eliminar_del_carrito, name='eliminar_del_carrito'),
+    path('restar/<int:bebida_id>/', restar_del_carrito, name='restar_del_carrito'),
+    path('sumar/<int:bebida_id>/', sumar_del_carrito, name='sumar_del_carrito'),
+]
---
+++ tienda/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Bebida
+from django.shortcuts import get_object_or_404, redirect
+from django.http import JsonResponse
+
+def lista_bebidas(request):
+    bebidas = Bebida.objects.all()
+    carrito = request.session.get('carrito', {})
+
+    return render(request, 'tienda/lista.html', {
+        'bebidas': bebidas,
+        'carrito': carrito
+    })
+
+def agregar_al_carrito(request, bebida_id):
+
+    carrito = request.session.get('carrito', {})
+
+    if str(bebida_id) in carrito:
+        carrito[str(bebida_id)] += 1
+    else:
+        carrito[str(bebida_id)] = 1
+
+    request.session['carrito'] = carrito
+
+    total_items = sum(carrito.values())
+
+    return JsonResponse({
+        "status": "ok",
+        "total_items": total_items
+    })
+
+def ver_carrito(request):
+    carrito = request.session.get('carrito', {})
+    bebidas = []
+    total = 0
+
+    for bebida_id, cantidad in carrito.items():
+        bebida = Bebida.objects.get(id=bebida_id)
+        subtotal = bebida.precio * cantidad
+        total += subtotal
+
+        bebidas.append({
+            'bebida': bebida,
+            'cantidad': cantidad,
+            'subtotal': subtotal
+        })
+
+    return render(request, 'tienda/carrito.html', {
+        'bebidas': bebidas,
+        'total': total
+    })
+
+def eliminar_del_carrito(request, bebida_id):
+    carrito = request.session.get('carrito', {})
+
+    if str(bebida_id) in carrito:
+        del carrito[str(bebida_id)]
+
+    request.session['carrito'] = carrito
+    return redirect('ver_carrito')
+
+def restar_del_carrito(request, bebida_id):
+
+    carrito = request.session.get('carrito', {})
+
+    if str(bebida_id) in carrito:
+
+        carrito[str(bebida_id)] -= 1
+
+        if carrito[str(bebida_id)] <= 0:
+            del carrito[str(bebida_id)]
+
+    request.session['carrito'] = carrito
+
+    return redirect('ver_carrito')
+
+def sumar_del_carrito(request, bebida_id):
+
+    carrito = request.session.get('carrito', {})
+
+    if str(bebida_id) in carrito:
+        carrito[str(bebida_id)] += 1
+    else:
+        carrito[str(bebida_id)] = 1
+
+    request.session['carrito'] = carrito
+
+    return redirect('ver_carrito')
