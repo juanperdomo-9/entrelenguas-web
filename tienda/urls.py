df30d1f2f260c73877a1060abd1108295450de3d
Juan Francisco  Perdomo <juanperdomo918918@gmail.com>
2026-03-05 17:55:04 -0300

    Mejora diseño galeria, carrito y estructura del proyecto

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,11 +4,6 @@ from .views import lista_bebidas, agregar_al_carrito, ver_carrito, eliminar_del_
 urlpatterns = [
     path('', lista_bebidas, name='lista_bebidas'),
     path('agregar/<int:bebida_id>/', agregar_al_carrito, name='agregar_al_carrito'),
-]
-
-urlpatterns = [
-    path('', lista_bebidas, name='lista_bebidas'),
-    path('agregar/<int:bebida_id>/', agregar_al_carrito, name='agregar_al_carrito'),
     path('carrito/', ver_carrito, name='ver_carrito'),
     path('eliminar/<int:bebida_id>/', eliminar_del_carrito, name='eliminar_del_carrito'),
     path('restar/<int:bebida_id>/', restar_del_carrito, name='restar_del_carrito'),
