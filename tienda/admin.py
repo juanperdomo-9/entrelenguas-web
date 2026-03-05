65e3536d2e97d148559fcd7901bf347dcbb0402b
Juan Francisco  Perdomo <juanperdomo918918@gmail.com>
2026-03-05 18:22:49 -0300

    Cambios

--- tienda/admin.py
+++ tienda/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Bebida
+from .models import Bebida, Pedido
 
 admin.site.register(Bebida)
+admin.site.register(Pedido)
--- tienda/models.py
+++ tienda/models.py
@@ -12,3 +12,18 @@ class Bebida(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Pedido(models.Model):
+
+    nombre_cliente = models.CharField(max_length=200)
+    email_cliente = models.EmailField()
+    direccion = models.CharField(max_length=300)
+
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    pagado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Pedido {self.id} - {self.nombre_cliente}"
