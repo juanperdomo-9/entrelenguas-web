e1e814009386307459ad6b13fd57fc7d6560c450
Juan Francisco  Perdomo <juanperdomo918918@gmail.com>
2026-03-09 13:46:07 -0300

    gmail

--- config/settings.py
+++ config/settings.py
@@ -159,10 +159,10 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_USER = "clientesportsidepm@gmail.com"
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = "clientesportsidepm@gmail.com"
 
 EMAIL_TIMEOUT = 30
 
